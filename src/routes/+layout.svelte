aef4e944c13f80c2767e57f296f341fcf7413b37
Kip <Kipling.Dunlap@gmail.com>
2025-01-14 23:03:10 -0500

    initial build

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<nav>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+    </ul>
+</nav>
+
+<slot/>
+
+<style>
+    nav {
+        background-color: #f0f0f0;
+        padding: 1rem;
+    }
+
+    ul {
+        display: flex;
+        gap: 1rem;
+        list-style: none;
+    }
+
+    li {
+        font-size: 1.25rem;
+    }
+
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<h1>Scan for my Business Card</h1>
+<p>Scan the QR code to get my contact details</p>
+<img width="200" style="margin: 30px;" src="./qrcode_kipdunlap.me.png" alt="QR Code">
+<p>Or enter your info so I can follow up with you!</p>
+<br>
+<form>
+  <label for="name">Name:</label>
+  <input type="text" id="name" name="name"><br>
+  <label for="email">Email:</label>
+  <input type="email" id="email" name="email">
+  <button type="submit">Submit</button>
+</form>
